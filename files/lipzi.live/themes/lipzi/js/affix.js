3975240070fee30fecb1cae367b1653e2f0c9246
Kevin Hausmann <admin@alliknow.net>
2017-04-21 10:37:24 +0200

    Refactor and clean-up, before move to Drupal 8.3.1

--- files/lipzi.live/themes/lipzi/js/affix.js
+++ files/lipzi.live/themes/lipzi/js/affix.js
@@ -1,3 +1,7 @@
+/* 
+ * This code will run once the page is loaded and make sure the main menu is not scrolled off.
+ * See https://www.w3schools.com/bootstrap/bootstrap_ref_js_affix.asp for details
+ */
 jQuery(document).ready(function(){
     jQuery('#block-lipzi-main-menu').affix({offset: {top: 300} });
 });
